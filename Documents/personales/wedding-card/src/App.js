ae227d3e69622ba5880769edee67564adedc0f45
ykarqui <ykarqui@gmail.com>
2021-10-22 13:36:31 -0300

    fix: descrition

--- Documents/personales/wedding-card/src/App.js
+++ Documents/personales/wedding-card/src/App.js
@@ -1,27 +1,28 @@
 // import logo from '../public/logo.png';
-import './App.css';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import WeadingDate from './components/WeadingDate';
-import CountDown from './components/CountDown';
-import Invitation from './components/Invitation';
-import Tarjeta from './components/Tarjeta'
-import Detalles from './components/Detalles';
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import WeadingDate from "./components/WeadingDate";
+import CountDown from "./components/CountDown";
+import Invitation from "./components/Invitation";
+import Tarjeta from "./components/Tarjeta";
+import Detalles from "./components/Detalles";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <WeadingDate />
-      <CountDown />
-      <Invitation />
-      {/* Tarjeta */}
-      <Tarjeta />
-      {/* Detalles */}
-      <Detalles /> 
-      <Contact />
-  </div>
-  );
-}
+    return (
+      <div className="App">
+        <Header />
+        <WeadingDate />
+        <CountDown />
+        <Invitation />
+        {/* Tarjeta */}
+        <Tarjeta />
+        {/* Detalles */}
+        <Detalles />
+        <Contact />
+      </div>
+    );
+};
+
 
 export default App;
