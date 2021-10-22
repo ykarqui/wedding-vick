4b14fb377c8bc23829e09ca3ecf10da0d84924aa
ykarqui <ykarqui@gmail.com>
2021-10-22 10:47:35 -0300

    feat: copypaste alias

--- Documents/personales/wedding-card/src/components/Tarjeta.js
+++ Documents/personales/wedding-card/src/components/Tarjeta.js
@@ -2,24 +2,38 @@ import React, { useState } from 'react';
 import '../WeadingDate.css';
 import { Button, Modal } from 'react-bootstrap';
 import {
-	FaRegEnvelopeOpen
+	FaRegEnvelopeOpen,
+    FaCopy
 } from 'react-icons/fa';
+import '../Tarjeta.css';
 
 const WeadingDate = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
-    const redirectTarjeta = () => {
-        window.location.href="https://mpago.la/1rXZUnz";
+    const getPriceOnMonth = () => {
+        const month = new Date().getMonth();
+
+        if ( month >= 2 ) {
+            this.setState({price: '5200'});;
+        }
+        if ( month === 0 ) {
+            this.setState({price: '5350'});;
+        }
+        
+        if ( month === 1 ) {
+            this.setState({price: '5500'});;
+        }
     }
-    
+
+    const [price, setPrice] = useState('5200');
+
     const redirectForm = () => {
         window.location.href="https://forms.gle/ebuTrw2tfTSzjuvL6";
     }
-    const btnStyle = {
-        'margin-bottom': '15px'
+    
+    const copyAlias = () => {
+        navigator.clipboard.writeText('JUAN.CRUZ.DUARTE');
     }
+
     return (
         <div className="main-slider">
             <div className="display-table center-text">
@@ -29,48 +43,30 @@ const WeadingDate = () => {
                     <h1 className="title"> Tarjeta </h1>
                     <br/><br/>
 
-                        <Button 
-                            variant="success" 
-                            onClick={handleShow} 
-                            size="lg"
-                            style={btnStyle}>
-                                Pagar por transferencia [$5000]
-                        </Button>
+                    <h3>Pagar por transferencia [$ {price} ]</h3>
                         
-                        <Button 
-                            onClick={ redirectTarjeta} 
-                            variant="primary" size="lg"
-                            title="Pagar con tarjeta $5500"
-                            style={btnStyle}>
-                                Pagar con tarjeta [$5500]
-                        </Button>
+                    Alias/CBU: JUAN.CRUZ.DUARTE
+                    
+                    <Button 
+                    className="btn-margin-left"
+                    onClick={ copyAlias }
+                    title="Notificar pago">
+                        <FaCopy/>
+                    </Button><br/>
+                    CA $ 928 0020275307 <br/>
+                    Nombre: JUAN CRUZ DUARTE MENOCHIO <br/>
+                    CUIL: 20399345104 <br/>
+                    CBU: 0200928911000020275376
+                    <br/><br/>
+                    <Button 
+                        onClick={ redirectForm }
+                        variant="link" 
+                        size="lg"
+                        title="Notificar pago">
+                            Notificar pago
+                    </Button>
                         
-                        
-                        <Button 
-                            onClick={ redirectForm}
-                            variant="success" 
-                            size="lg"
-                            title="Notificar pago"
-                            style={btnStyle}>
-                                Notificar pago
-                        </Button>
-
-                        <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                            <Modal.Title>Alias/CBU: JUAN.CRUZ.DUARTE</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                            CA $ 928 0020275307 <br/>
-                            Nombre: JUAN CRUZ DUARTE MENOCHIO <br/>
-                            CUIL: 20399345104 <br/>
-                            CBU: 0200928911000020275376
-                            </Modal.Body>
-                            <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Cerrar
-                            </Button>
-                            </Modal.Footer>
-                        </Modal>
+                            
                     </div>
 			</div>
 		</div>
