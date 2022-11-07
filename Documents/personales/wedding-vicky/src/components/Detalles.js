fa35b58c986a14fb3d8106f01d35be399d55838c
ykarqui <ykarqui@gmail.com>
2022-11-07 18:59:47 -0300

    fix frame

--- Documents/personales/wedding-vicky/src/components/Detalles.js
+++ Documents/personales/wedding-vicky/src/components/Detalles.js
@@ -33,7 +33,7 @@ const Detalles = () => {
 							<br/>
 							<div>
 								<div className="map-responsive">
-									<iframe <iframe 
+									<iframe
 	    									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18130.10271791835!2d-64.29504420859331!3d-31.301577481221855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329d056aa66e5f%3A0x8d59cccda2c33d4f!2sIglesia%20Nuestra%20Se%C3%B1ora%20del%20Carmen!5e0!3m2!1ses-419!2sar!4v1667857984811!5m2!1ses-419!2sar"
 										width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" title="myFrame"></iframe>
 								</div>
